9ff101e5f7b1654fa1cd0807fa24572a91b0106d
Manohar <mano55eswara@gmail.com>
2026-09-13 04:16:22 +0530

    Revert "Level 3: laser walls, hidden timer, loop reset trigger, timed door puzzle"
    This reverts commit 03ff0a8cd04d5ffbe23be738008d32919ec0433f.

--- Assets/LoopManager.cs
+++ Assets/LoopManager.cs
@@ -109,12 +109,4 @@ public class LoopManager : MonoBehaviour
     {
         return timer;
     }
-
-    /// <summary>
-    /// Called by LoopResetTrigger to immediately end the current recording and spawn a ghost.
-    /// </summary>
-    public void ForceResetLoop()
-    {
-        ResetLoop();
-    }
 }
