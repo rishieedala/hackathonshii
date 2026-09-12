1889686219994bdbb9f083a36a1aff64247b2778
Manohar <mano55eswara@gmail.com>
2026-09-13 04:15:51 +0530

    Revert "change"
    This reverts commit 8f89d8c600bd6542bffe2b3ff54a05efd34f60dd.

--- Assets/WinScreen.cs
+++ Assets/WinScreen.cs
@@ -20,10 +20,4 @@ public class WinScreen : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("Level2");
     }
-
-    public void GoToLevel3()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Level3");
-    }
 }
